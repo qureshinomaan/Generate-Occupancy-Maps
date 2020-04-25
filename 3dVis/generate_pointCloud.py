2a7f2b745d7a7b2af88167abeca4270b384ae31e
qureshi.nomaan <nqafzal@gmail.com>
2020-04-25 08:12:12 +0530

    3d Generation In progress

--- 3dVis/generate_pointCloud.py
+++ 3dVis/generate_pointCloud.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-image_seg = cv2.imread("../inputs/left.png")
+image_seg = cv2.imread("../outputs/instance_segmentation/IS_left.jpg")
 image_depth = cv2.imread('../disparity.png')
 gray = cv2.cvtColor(image_depth, cv2.COLOR_RGB2GRAY)
 
@@ -13,5 +13,5 @@ f=open("PointCloud.pcd", "a+")
 for x in range(gray.shape[0]):
     for y in range(gray.shape[1]):
         color =  65536 * image_seg[x][y][0] + 256 * image_seg[x][y][1] + image_seg[x][y][2]
-        f.write(str(x) + " " + str(y) + " " + str(gray[x][y]*10) + " " +str(color) +"\n" )
+        f.write(str(x) + " " + str(y) + " " + str(gray[x][y]*25) + " " +str(color) +"\n" )
 f.close()
