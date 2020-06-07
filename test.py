ac80e09a8fd00ac3f4c6ba4644481d3b99d7811f
qureshi.nomaan <nqafzal@gmail.com>
2020-06-07 16:54:20 +0530

    Removing some unnecessary files

--- test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import sys
-sys.path.insert(1, 'models/PSMNet')
-from generate_disparity import generate_disparity
-
-left = cv2.imread('left.png')
-right = cv2.imread('right.png')
-generate_disparity(left, right, False)
